8305806fd4205ad76831c86ca9e4b25fdcfa796a
taras-kozak <kozaktaras15@gmail.com>
2020-05-05 13:05:05 -0400

    added shoping list get path to backend

--- recipe-box-api/src/routers/shopingList.js
+++ recipe-box-api/src/routers/shopingList.js
@@ -3,26 +3,41 @@ const ShopingList=require('../models/shopingList');
 const router=new express.Router();
 const auth=require('../middleware/auth');
 
+router.get('/shoping-list', auth, async (req, res) => {
+    try{
+    const shopingList = await ShopingList.findOne({ owner: req.user.authID})
+    console.log('shopping list: ', shopingList)
+
+    if (!shopingList) {
+        shopingList=new ShopingList({
+            shopingList:[], //for now just creates an empty shopping list
+            owner: req.user.authID
+        })
+        await shopingList.save()
+        res.status(201).send()
+    }
+    else{
+    
+    res.status(201).send(shopingList.ShopingList)
+    }
+
+} catch (e) {
+    res.status(500).send(e)
+}
+})
+
 router.patch('/shoping-list', auth, async (req, res) => {
     const updatedShopingList=JSON.parse(req.body.shopingList)
     
     try {
         const shopingList = await ShopingList.findOne({ owner: req.user.authID})
 
-        if (!shopingList) {
-            shopingList=new ShopingList({
-                shopingList:[...updatedShopingList], //for now just creates an empty shopping list
-                owner: req.user.authID
-            })
-            await shopingList.save()
-            res.status(201).send()
-        }
-        else{
+        
         shopingList.list=updatedShopingList
 
         await shopingList.save()
-        res.status(201).send()
-        }
+        res.status().send()
+        
 
     } catch (e) {
         res.status(400).send(e)
---
+++ src/redux/shopingList/shopingList.sagas.js
@@ -0,0 +1,5 @@
+import {takeLatest, put,all, call} from 'redux-saga/effects';
+import axios from 'axios';
+
+const shopingListAPIpath='http://localhost:3001/shoping-list'
+
