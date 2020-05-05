c4f4a26e94d6ab3865e260e311183dc08b8a55f6
taras-kozak <kozaktaras15@gmail.com>
2020-05-05 13:22:15 -0400

    added shopingList sagas

--- src/redux/shopingList/shopingList.sagas.js
+++ src/redux/shopingList/shopingList.sagas.js
@@ -1,5 +1,45 @@
 import {takeLatest, put,all, call} from 'redux-saga/effects';
 import axios from 'axios';
+import {fetchShopingListSuccess, fetchShopingListFailure, updateShopingListSuccess, updateShopingListFailure} from '../shopingList/shopingList.actions'
+import ShopingListActionTypes from '../shopingList/shopingList.types'
 
 const shopingListAPIpath='http://localhost:3001/shoping-list'
 
+function* FetchShopingList({payload}){
+    try{
+        yield axios.defaults.headers.common['userID'] =payload 
+        const shopingList=yield axios.get(shopingListAPIpath)
+        
+        yield put(fetchShopingListSuccess(shopingList))
+    }
+    catch(error){
+       yield put(fetchShopingListFailure(error))
+    }
+
+}
+
+function* UpdateShopingList({payload}){
+    const shopingListObj={
+        shopingList:payload
+    }
+    try{
+        yield axios.post(shopingListAPIpath,shopingListObj).then((response)=>console.log(response))
+        yield put(updateShopingListSuccess(payload))
+    }
+    catch(error){
+       yield put(updateShopingListFailure(error.message))
+    }
+}
+
+
+function* onShopingListFetchStart(){
+    yield takeLatest(ShopingListActionTypes.FETCH_ShopingList_START, FetchShopingList)
+}
+
+function* onShopingListUpdateStart(){
+    yield takeLatest(ShopingListActionTypes.UPDATE_ShopingList_START, UpdateShopingList)
+}
+
+export function* shopingListSagas(){
+    yield all([call(onShopingListFetchStart), call(onShopingListUpdateStart)])
+}
