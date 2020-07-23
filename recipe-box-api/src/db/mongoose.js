266e92f4fd46261a95e17261ce9fbb8628451f53
Taras <kozaktaras15@gmail.com>
2020-07-23 10:46:17 -0400

    update mongose connection to solve timeout issue.  Update nginx config to resolve issue with react-router

--- recipe-box-api/src/db/mongoose.js
+++ recipe-box-api/src/db/mongoose.js
@@ -4,6 +4,14 @@ mongoose.connect('mongodb+srv://adminUser:iko12345@cluster0.zdb1j.mongodb.net/re
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    replset: {
+        socketOptions: {
+            connectTimeoutMS: 300000, // 5 minutes
+            keepAlive: 120
+        },
+        ha: true, // Make sure the high availability checks are on
+        haInterval: 10000, // Run every 10 seconds
+    }
     
 })
